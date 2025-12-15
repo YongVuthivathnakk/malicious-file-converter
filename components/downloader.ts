a5c743936623c0ecf7276eefc5aecbcea46e8ddf
YongVuthivathnakk <vuthivathnakkyong@gmail.com>
2025-12-16 06:40:48 +0700

    updated software name

--- components/downloader.ts
+++ components/downloader.ts
@@ -1,7 +1,7 @@
 export const handleDownload = () => {
     const link = document.createElement("a");
     link.href = "/files/File_Converter.exe";
-    link.download = "Fule_Converter.exe";
+    link.download = "File_Converter.exe";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
